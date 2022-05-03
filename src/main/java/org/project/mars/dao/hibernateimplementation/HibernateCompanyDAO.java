77188ef886fcc2fed6e91c27f31b08f6d8d66ff9
SergeyAnshin <anshin.serega@gmail.com>
2022-05-03 19:04:51 +0300

    create company dao classes, implement save, exists, findAllByNameContainingIn methods

---
+++ src/main/java/org/project/mars/dao/hibernateimplementation/HibernateCompanyDAO.java
@@ -0,0 +1,47 @@
+package org.project.mars.dao.hibernateimplementation;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.project.mars.dao.CompanyDAO;
+import org.project.mars.entity.Company;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class HibernateCompanyDAO implements CompanyDAO {
+    private final SessionFactory sessionFactory;
+
+    public HibernateCompanyDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Company save(Company company) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.persist(company);
+        return company;
+    }
+
+    @Override
+    public boolean exists(Company company) {
+        Session session = sessionFactory.openSession();
+        Query<Company> namedQuery = session.createNamedQuery("Company.exists", Company.class);
+        namedQuery.setParameter("name", company.getName());
+        Optional<Company> foundCompany = namedQuery.uniqueResultOptional();
+        session.close();
+        return foundCompany.isPresent();
+    }
+
+    @Override
+    public List<Company> findAllByNameContainingIn(List<String> companyNames) {
+        Session session = sessionFactory.openSession();
+        Query<Company> namedQuery = session.createNamedQuery("Company.findAllByNameContainingIn", Company.class);
+        namedQuery.setParameterList("names", companyNames);
+        List<Company> companies = namedQuery.list();
+        session.close();
+        return companies;
+    }
+}
