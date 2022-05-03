c185acc856da55b5f9bcfb1e6866f790c1995dd4
SergeyAnshin <anshin.serega@gmail.com>
2022-05-03 18:58:07 +0300

    add companies, positions, skills in init data file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,14 @@ INSERT INTO users(creationDateTime, updateDateTime, email, firstName, lastName, 
 INSERT INTO users_roles(user_id, role_id) VALUE (1,2);
 INSERT INTO users(creationDateTime, updateDateTime, email, firstName, lastName, password, username) VALUE (NOW(), NOW(), 'admin@gmail.com', 'admin', 'admin', '$2a$12$Yeu9s3uMxRx/k4KUjTtnEe7RZFGmuBw6iaAPHiC8CzF31cIGbDJFa', 'admin');
 INSERT INTO users_roles(user_id, role_id) VALUE (2,1);
+
+# SKILLS
+INSERT INTO skill(creationDateTime, updateDateTime, name) VALUE (NOW(), NOW(), 'Java');
+INSERT INTO skill(creationDateTime, updateDateTime, name) VALUE (NOW(), NOW(), 'OOP');
+INSERT INTO skill(creationDateTime, updateDateTime, name) VALUE (NOW(), NOW(), 'Hibernate');
+
+# COMPANIES
+INSERT INTO company(creationDateTime, updateDateTime, name, site) VALUE (NOW(), NOW(), 'Google', 'google.com');
+
+# POSITIONS
+INSERT INTO position_table(creationDateTime, updateDateTime, name) VALUE (NOW(), NOW(), 'Java developer');
