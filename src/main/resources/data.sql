1ef126b4b4647e8bde17e4ced0caaa2f2e9ab3d3
SergeyAnshin <anshin.serega@gmail.com>
2022-05-06 20:05:56 +0300

    add role and employer to init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@
 INSERT INTO role(creationDateTime, updateDateTime, name) VALUE (NOW(),NOW(),'ADMIN');
 INSERT INTO role(creationDateTime, updateDateTime, name) VALUE (NOW(),NOW(),'USER');
 INSERT INTO role(creationDateTime, updateDateTime, name) VALUE (NOW(),NOW(),'GUEST');
+INSERT INTO role(creationDateTime, updateDateTime, name) VALUE (NOW(),NOW(),'EMPLOYER');
 
 # USERS
 INSERT INTO users(creationDateTime, updateDateTime, email, firstName, lastName, password, username) VALUE (NOW(), NOW(), 'user@gmail.com', 'user', 'user', '$2a$12$Kh8lJ69CilLhT6paLNP46.gYRp95KtW80t99L9XJki0THpiUCQOSG', 'user');
@@ -19,3 +20,7 @@ INSERT INTO company(creationDateTime, updateDateTime, name, site) VALUE (NOW(), 
 
 # POSITIONS
 INSERT INTO position_table(creationDateTime, updateDateTime, name) VALUE (NOW(), NOW(), 'Java developer');
+
+# EMPLOYERS
+INSERT INTO employer(creationDateTime, updateDateTime, email, firstName, lastName, password, phoneNumber, company_id) VALUE (NOW(), NOW(), 'em@gmail.com', 'em', 'em', '$2a$12$tEGY3Cz9/aCv78qWm9rStet0O9Py56lrRTC..fhmiSh87.wERho8K', '+375330001122', 1);
+INSERT INTO employers_roles(employer_id, role_id) VALUE (1, 4);
