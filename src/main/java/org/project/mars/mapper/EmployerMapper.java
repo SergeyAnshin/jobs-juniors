0c720553f41909f7bcc7717d4ce46cde487fa5d0
SergeyAnshin <anshin.serega@gmail.com>
2022-05-07 17:48:35 +0300

    change employer principal

--- src/main/java/org/project/mars/mapper/EmployerMapper.java
+++ src/main/java/org/project/mars/mapper/EmployerMapper.java
@@ -3,9 +3,12 @@ package org.project.mars.mapper;
 import org.project.mars.dto.RegisteringEmployer;
 import org.project.mars.entity.Company;
 import org.project.mars.entity.Employer;
+import org.project.mars.entity.Role;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class EmployerMapper {
     private static final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
@@ -21,6 +24,28 @@ public class EmployerMapper {
                     .email(registeringEmployer.getEmail())
                     .company(Company.builder().name(registeringEmployer.getCompanyName()).build())
                     .roles(new HashSet<>())
+                    .build();
+        }
+    }
+
+    public static UserDetails mapToUserDetails(Employer employer) {
+        if (employer == null) {
+            return null;
+        } else {
+            return Employer.builder()
+                    .id(employer.getId())
+                    .email(employer.getEmail())
+                    .password(employer.getPassword())
+                    .company(Company.builder()
+                            .id(employer.getCompany().getId())
+                            .name(employer.getCompany().getName())
+                            .build())
+                    .roles(employer.getRoles()
+                            .stream()
+                            .map(role -> Role.builder()
+                                    .name(role.getName())
+                                    .build())
+                            .collect(Collectors.toSet()))
                     .build();
         }
     }
