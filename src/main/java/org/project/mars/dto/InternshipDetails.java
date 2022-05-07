6d1c19e54daff7de0f9564683d373c9d880ef4b4
SergeyAnshin <anshin.serega@gmail.com>
2022-05-07 17:52:28 +0300

    change internship details

--- src/main/java/org/project/mars/dto/InternshipDetails.java
+++ src/main/java/org/project/mars/dto/InternshipDetails.java
@@ -3,10 +3,10 @@ package org.project.mars.dto;
 import lombok.*;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.project.mars.validation.constraints.AfterNow;
 import org.project.mars.validation.constraints.BeforeNow;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Future;
 import java.time.LocalDate;
 
 @NoArgsConstructor
@@ -22,10 +22,9 @@ public class InternshipDetails {
     @NotEmpty
     @NotBlank
     private String technology;
-    @BeforeNow(message = "Date must be before today!")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
-    @Future
+    @AfterNow(message = "Date must be after today!")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate finishDate;
     @NotEmpty
@@ -40,4 +39,5 @@ public class InternshipDetails {
     @NotEmpty
     @NotBlank
     private String contact;
+    private String companyName;
 }
