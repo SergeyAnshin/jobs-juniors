716d00552d636d7344fb6f90149a15d0626649d4
SergeyAnshin <anshin.serega@gmail.com>
2022-05-07 17:34:14 +0300

    create AfterNow validation constraint

---
+++ src/main/java/org/project/mars/validation/constraints/AfterNow.java
@@ -0,0 +1,33 @@
+package org.project.mars.validation.constraints;
+
+import org.project.mars.validation.AfterNowValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = AfterNowValidator.class)
+public @interface AfterNow {
+
+    String message() default "{}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        AfterNow[] value();
+    }
+}
