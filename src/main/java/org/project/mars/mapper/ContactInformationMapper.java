73d3127a762ae0fc05a00bdd306ce8e83e34d7e9
SergeyAnshin <anshin.serega@gmail.com>
2022-05-11 16:27:42 +0300

    add map to and from GeneralInformationDetails

--- src/main/java/org/project/mars/mapper/ContactInformationMapper.java
+++ src/main/java/org/project/mars/mapper/ContactInformationMapper.java
@@ -1,5 +1,6 @@
 package org.project.mars.mapper;
 
+import org.project.mars.dto.GeneralInformationDetails;
 import org.project.mars.dto.ResumeDTO;
 import org.project.mars.entity.ContactInformation;
 
@@ -16,6 +17,22 @@ public class ContactInformationMapper {
                     .linkedinLink(resumeDTO.getLinkedinLink())
                     .githubLink(resumeDTO.getGithubLink())
                     .location(resumeDTO.getLocation())
+                    .build();
+        }
+    }
+
+    public static ContactInformation mapFromGeneralInformationDetails(GeneralInformationDetails generalInformationDetails) {
+        if (generalInformationDetails == null) {
+            return null;
+        } else {
+            return ContactInformation.builder()
+                    .id(generalInformationDetails.getContactId())
+                    .email(generalInformationDetails.getEmail())
+                    .telegramLink(generalInformationDetails.getTelegramLink())
+                    .phoneNumber(generalInformationDetails.getPhoneNumber())
+                    .linkedinLink(generalInformationDetails.getLinkedinLink())
+                    .githubLink(generalInformationDetails.getGithubLink())
+                    .location(generalInformationDetails.getLocation())
                     .build();
         }
     }
